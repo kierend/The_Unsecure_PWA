9a5ef516f9553fd3dcbe3d4b90d079c9af5e89fb
Kieren Dowding <35049620+kierend@users.noreply.github.com>
2025-03-06 23:18:33 +1100

    fixed external redirect

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+from flask_csp.csp import csp_header
 import user_management as dbHandler
 
 # Code snippet for logging a message
@@ -9,12 +10,13 @@ import user_management as dbHandler
 
 app = Flask(__name__)
 
-
 @app.route("/success.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
+@csp_header()
 def addFeedback():
+
     if request.method == "GET" and request.args.get("url"):
         url = request.args.get("url", "")
-        return redirect(url, code=302)
+        return checkRedirect(url)
     if request.method == "POST":
         feedback = request.form["feedback"]
         dbHandler.insertFeedback(feedback)
@@ -26,10 +28,11 @@ def addFeedback():
 
 
 @app.route("/signup.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
+@csp_header()
 def signup():
     if request.method == "GET" and request.args.get("url"):
         url = request.args.get("url", "")
-        return redirect(url, code=302)
+        return checkRedirect(url)
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -42,10 +45,11 @@ def signup():
 
 @app.route("/index.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
 @app.route("/", methods=["POST", "GET"])
+@csp_header()
 def home():
     if request.method == "GET" and request.args.get("url"):
         url = request.args.get("url", "")
-        return redirect(url, code=302)
+        return checkRedirect(url)
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -58,6 +62,11 @@ def home():
     else:
         return render_template("/index.html")
 
+def checkRedirect(url):
+    if (url.startswith("/")):
+        return redirect(url, code=302)
+    else:
+        return abort(500)
 
 if __name__ == "__main__":
     app.config["TEMPLATES_AUTO_RELOAD"] = True
